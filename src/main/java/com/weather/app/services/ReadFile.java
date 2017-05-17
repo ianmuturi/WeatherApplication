3901d52ca3c48ee1946825ef817681beda90761d
ian kinyua <ian kinyua>
2017-05-17 12:08:53 +0300

    First Commit

---
+++ src/main/java/com/weather/app/services/ReadFile.java
@@ -0,0 +1,116 @@
+package com.weather.app.services;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.weather.app.WeatherAppApplication;
+import com.weather.app.utils.Weather;
+
+
+@Component
+public class ReadFile {
+
+	@Autowired
+	MaxSpread maxSpread;
+	
+	Weather weather = new Weather();
+	
+	
+	@EventListener(ContextRefreshedEvent.class)
+	public void readWeatherFile() throws IOException 
+	{
+		//Initialize
+		String fileName= "weather.dat";
+		Scanner inputStream;		 
+		URL path = WeatherAppApplication.class.getResource(fileName);
+		
+		//Gets the file with the weather data
+        File file= new File(path.getFile());
+
+        try{
+        	//Scanner will read from file
+            inputStream = new Scanner(file);
+            
+            //Read header lines
+            inputStream.nextLine();
+            inputStream.nextLine();
+            
+            //Read body lines from the file
+            while(inputStream.hasNextLine()){
+            	            	
+            	//Store line in a variable
+                String line= inputStream.nextLine();
+                
+                //Skip footer line
+            	if (line.contains("mo")) {
+  			      continue;
+  			    }
+                
+                //Format line to remove spaces
+                line = line.replace("  "," ");
+                line = line.replace("  "," ");
+                line = line.replace("  "," ");                
+                line = line.trim();
+                
+                //Replace single space with comma
+                line = line.replace(" ",",");
+                
+                
+                //Split the line into parts
+                String []values = line.split(",");
+                
+                //Get components of the parts
+                String strDay = values[0];
+                String strMaxTemp = values[1];
+                String strMinTemp = values[2];
+            	
+                //set values of file to weather object
+                weather.setDay(strDay);
+                
+                //find integer within a string
+                Pattern p = Pattern.compile("[0-9]+");
+                Matcher m = p.matcher(strMinTemp);
+                while (m.find()) {
+                    int n = Integer.parseInt(m.group());
+                  // set integer to weather
+                    weather.setMinTemperature(n);
+                }
+                
+               //find integer within a string
+                Pattern p1 = Pattern.compile("[0-9]+");
+                Matcher m1 = p1.matcher(strMaxTemp);
+                while (m1.find()) {
+                    int n = Integer.parseInt(m1.group());
+                    // set integer to weather
+                    weather.setMaxTemperature(n);
+                }
+                
+                //set temperature spread
+                weather.setTempSpread(weather.getMaxTemperature()-weather.getMinTemperature());
+                
+                
+            }
+
+            System.out.println("*******************   Start Reading   ******************");
+            //Print the maximum temperature spread and day
+            System.out.println("Day: "+weather.getDay()+"  Maximum Temperature Spread:  "+maxSpread.getMax(weather));
+            System.out.println("******************* Finished Reading *******************");
+            inputStream.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        
+	}
+	
+}
